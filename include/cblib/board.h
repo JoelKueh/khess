715922a1310314ef553476a124ef113bd7346b20
Joel Kuehne <kuehn348@umn.edu>
2025-03-22 09:24:16 -0500

    Saving before deleting files

--- include/cblib/board.h
+++ include/cblib/board.h
@@ -37,6 +37,10 @@ int cb_board_from_pgn(cb_board_t *board, const char *pgn);
 int cb_board_to_str(char str_rep[8][8], const cb_board_t *board);
 int cb_board_to_utf8(char str_rep[8][8], const cb_board_t *board);
 
+/* Functions for making and unmaking moves. */
+void make(cb_board_t *board, cb_move_t mv);
+void unmake(cb_board_t *board);
+
 /* Functions for reading the board representation. */
 inline cb_pid_t cb_ptype_at_sq(cb_board_t *board, uint8_t sq)
 {
@@ -51,7 +55,7 @@ inline cb_pid_t cb_ptype_at(cb_board_t *board, uint8_t row, uint8_t col)
 inline cb_pid_t cb_pid_at_sq(cb_board_t *board, uint8_t sq)
 {
     assert(false && "not yet implemented");
-    return board->mb.data[sq] * ;
+    return board->mb.data[sq];
 }
 
 inline cb_pid_t cb_pid_at(cb_board_t *board, uint8_t row, uint8_t col)
@@ -60,14 +64,41 @@ inline cb_pid_t cb_pid_at(cb_board_t *board, uint8_t row, uint8_t col)
     return cb_pid_at_sq(board, row * 8 + col);
 }
 
-inline cb_pid_t cb_color_at_sq(cb_board_t *board, uint8_t sq)
+inline cb_color_t cb_color_at_sq(cb_board_t *board, uint8_t sq)
 {
-    return board->bb.color[C_WHITE]
+    return board->bb.color[CB_WHITE] & (UINT64_C(1) << sq) ? CB_WHITE : CB_BLACK;
 }
 
-inline cb_pid_t cb_color_at(cb_board_t *board, uint8_t row, uint8_t col)
+inline cb_color_t cb_color_at(cb_board_t *board, uint8_t row, uint8_t col)
 {
     return cb_color_at_sq(board, row * 8 + col);
+}
+
+/* Functions for manipulating the board representation. */
+inline void cb_replace_piece(cb_board_t *board, uint8_t sq, uint8_t ptype, uint8_t pcolor,
+        uint8_t old_ptype, uint8_t old_pcolor)
+{
+    board->mb.data[sq] = ptype;
+    board->bb.piece[pcolor][ptype] |= UINT64_C(1) << sq;
+    board->bb.color[pcolor] |= UINT64_C(1) << sq;
+    board->bb.piece[old_pcolor][old_ptype] &= ~(UINT64_C(1) << sq);
+    board->bb.color[old_pcolor] &= ~(UINT64_C(1) << sq);
+}
+
+inline void cb_write_piece(cb_board_t *board, uint8_t sq, uint8_t ptype, uint8_t pcolor)
+{
+    board->mb.data[sq] = ptype;
+    board->bb.piece[pcolor][ptype] |= UINT64_C(1) << sq;
+    board->bb.color[pcolor] |= UINT64_C(1) << sq;
+    board->bb.occ |= UINT64_C(1) << sq;
+}
+
+inline void cb_delete_piece(cb_board_t *board, uint8_t sq, uint8_t ptype, uint8_t pcolor)
+{
+    board->mb.data[sq] = BB_EMPTY;
+    board->bb.piece[pcolor][ptype] &= ~(UINT64_C(1) << sq);
+    board->bb.color[pcolor] &= ~(UINT64_C(1) << sq);
+    board->bb.occ &= ~(UINT64_C(1) << sq);
 }
 
 #endif /* CB_BOARD_H */
